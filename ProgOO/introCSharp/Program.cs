04826b3e7d2af196db24c2097fa3a36e68aaec75
opaul-hus <1156001@cstj.qc.ca>
2023-01-23 17:05:02 -0500

    premier programme

---
+++ ProgOO/introCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿//Les commentaires sont comme en cpp
+/* Les commentaires multi lignes */
+
+
+// notre premier programe en c#
+
+
+// en c# il a un seul type de fichier, les headers n'existe pas
+
+//au lieu de using namespace std;
+using System;
+// le using name space nest pas necessaire
+
+
+//pas de directives #include <..>
+
+//pas bessoin de set locale
+
+//pas de fonction main on ecrit les instructions principales des le debut du fichier
+
+// afficher dans la console
+System.Console.WriteLine("bonjour patate");
+
+Console.Write(" chips");
